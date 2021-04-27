f9da876552dafcea496a45edd45588f6064744ac
Johandré Bothma <2017126596@ufs4life.ac.za>
2021-04-27 17:57:35 +0200

    End of section 10 (Claims problem in UpdateUser)

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,22 +1,41 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if (this.members.length > 0) { return of(this.members); }//If the members were already requested before
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    );//If this is the first request for members, get it from the API
   }
 
   getMember(username: string) {
+    const member = this.members.find(x => x.username === username);
+    if (member !== undefined) { return of(member); }
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    );
   }
 }
